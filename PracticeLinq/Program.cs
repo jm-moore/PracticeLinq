e18987721a3f06e2f3ab3379d9fda18d921152d9
jm-moore <jmmoore9358@gmail.com>
2022-08-26 11:14:33 -0500

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,2 @@
+﻿var videoGames = new List<string>() {"Dragon Age: Inquisition", "Destiny", "Apex: Legends", "Call of Duty", "Halo", "Assassin's Creed" };
+videoGames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
